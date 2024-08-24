8291142466a3fff6390e6663a2c6ab0a88c068c5
Kaviya K <94157353+Kaviyakalaiselvam@users.noreply.github.com>
2024-08-24 14:02:00 +0530

    Create in and out.java

---
+++ in and out.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int b=scan.nextInt();
+        int c=scan.nextInt();
+        int d=scan.nextInt();
+        scan.close();
+        
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+                
+    }
+}
